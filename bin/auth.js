8232225c7b79de039281dda6cfccb6bc4ba97093
Wei Wang <ww@9rivers.com>
2022-10-25 01:13:11 -0400

    Changed MSAL config to use cert

--- bin/auth.js
+++ bin/auth.js
@@ -1,22 +1,25 @@
 const msal = require('@azure/msal-node');
 
 /**
- * Configuration object to be passed to MSAL instance on creation. 
+ * Configuration object to be passed to MSAL instance on creation.
  * For a full list of MSAL Node configuration parameters, visit:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-node/docs/configuration.md 
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-node/docs/configuration.md
  */
 const msalConfig = {
 	auth: {
 		clientId: process.env.CLIENT_ID,
 		authority: process.env.AAD_ENDPOINT + process.env.TENANT_ID,
-		clientSecret: process.env.CLIENT_SECRET,
+		clientCertificate: {
+			thumbprint: process.env.CERT_THUMBPRINT,
+			privateKey: process.env.CERT_PRIVATE_KEY,
+		}
 	}
 };
 
 /**
  * With client credentials flows permissions need to be granted in the portal by a tenant administrator.
- * The scope is always in the format '<resource-appId-uri>/.default'. For more, visit: 
- * https://docs.microsoft.com/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow 
+ * The scope is always in the format '<resource-appId-uri>/.default'. For more, visit:
+ * https://docs.microsoft.com/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow
  */
 const tokenRequest = {
 	scopes: [process.env.GRAPH_ENDPOINT + '.default'], // e.g. 'https://graph.microsoft.com/.default'
@@ -34,7 +37,7 @@ const cca = new msal.ConfidentialClientApplication(msalConfig);
 
 /**
  * Acquires token with client credentials.
- * @param {object} tokenRequest 
+ * @param {object} tokenRequest
  */
 async function getToken(tokenRequest) {
 	return await cca.acquireTokenByClientCredential(tokenRequest);
